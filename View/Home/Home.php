c86f9948070a70a7a3db82dddd384b8f5072111c
Patric Alison <alisson_gamer2000@outlook.com>
2023-02-26 11:02:38 -0300

    update and add elements of page home.php

--- View/Home/Home.php
+++ View/Home/Home.php
@@ -1,1 +1,72 @@
-<h1>Teste Home</h1>
+<!--Search-->
+<section class="container-fluid my-3">
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <form class="d-flex mx-auto" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</section>
+
+<section class="container-fluid my-1">
+    <table class="table text-bg-light">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry the Bird</td>
+                <td>Thornton</td>
+                <td>@twitter</td>
+            </tr>
+
+            <tr>
+                <th scope="row">4</th>
+                <td>Larry the Bird</td>
+                <td>Thornton</td>
+                <td>@twitter</td>
+            </tr>
+
+            <tr>
+                <th scope="row">5</th>
+                <td>Larry the Bird</td>
+                <td>Thornton</td>
+                <td>@twitter</td>
+            </tr>
+
+            <tr>
+                <th scope="row">6</th>
+                <td>Larry the Bird</td>
+                <td>Thornton</td>
+                <td>@twitter</td>
+            </tr>
+
+            <tr>
+                <th scope="row">7</th>
+                <td>Larry the Bird</td>
+                <td>Thornton</td>
+                <td>@twitter</td>
+            </tr>
+        </tbody>
+    </table>
+</section>
