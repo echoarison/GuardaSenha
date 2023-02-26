2fdd221a0ccb901ff36e73e2a0d0f4eb61c243ea
Patric Alison <alisson_gamer2000@outlook.com>
2023-02-26 08:05:31 -0300

    add header and footer in folder View

---
+++ View/Footer.php
@@ -0,0 +1,8 @@
+    <footer class="container-fluid">
+        <p style="text-align:center">
+            Guarda Senha 2023
+        </p>
+    </footer>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -3,29 +3,13 @@
     ini_set('error_reporting', E_ALL); // mesmo resultado de: error_reporting(E_ALL);
     ini_set('display_errors', 1);
 
-    //chamando o model
-    require_once './Model/Configuracao.php';
-    require_once './Model/MasterPass.php';
+    //chamando o header
+    require_once "./View/Header.php";
+?>
 
-    //fazendo teste de class
-    $testeMaster = new MasterPass();
+<h1><marquee>Hello World</marquee></h1>
 
-    //instanciando MastetPass
-    $testeMaster->set_passCript("admin1234");
-
-    //guardando o valor
-
-    echo "Class MasterPass : " . $testeMaster->get_passCript();
-
-    $guardTest = $testeMaster->get_passCript();
-
-    echo "<br>" . gettype($guardTest);
-    //iniciando
-    $testeConfig = new Configuracao($guardTest,true);
-
-    //instanciando Configuracao
-    //$testeConfig->set_passWord($guardTest);
-
-    echo "<br>Acessando pelo Configuracao: " . $testeConfig->get_passWord();
-    
+<?php
+    //chamando o footer
+    require_once "./View/Footer.php";
 ?>
