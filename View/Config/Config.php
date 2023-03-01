6470488b4a099c079704be8cacb6f364c3c5f52a
Patric Alison <alisson_gamer2000@outlook.com>
2023-03-01 19:19:22 -0300

    add page view config.php

--- View/Config/Config.php
+++ View/Config/Config.php
@@ -1,1 +1,61 @@
-<h1>Hello world!</h1>
+<section class="conteiner-fluid">
+
+    <div class="card my-3">
+        <h5 class="card-header">Você quer ativa a senha ?</h5>
+        <div class="card-body">
+            <div class="form-check form-check-inline">
+
+            <input class="form-check-input" type="checkbox" value="Nao" id="flexCheckDefault" checked>
+                <label class="form-check-label" for="flexCheckDefault">
+                    Não
+                </label>
+
+            </div>
+
+            <div class="form-check form-check-inline">
+
+                <input class="form-check-input" type="checkbox" value="Sim" id="flexCheckChecked">
+                <label class="form-check-label" for="flexCheckChecked">
+                    Sim
+                </label>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="card my-3">
+        <h5 class="card-header">Alterar Senha:</h5>
+        <div class="card-body">
+            <div class="mb-3">
+                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="New password">
+                <button type="submit" class="btn btn-primary my-2">Alterar</button>
+            </div>
+        </div>
+    </div>
+    
+    <div class="card my-3">
+        <h5 class="card-header">Nivel de Criptografia</h5>
+        <div class="card-body">
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked>
+                <label class="form-check-label" for="flexRadioDefault1">
+                    Só a Senha
+                </label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                <label class="form-check-label" for="flexRadioDefault2">
+                    Os Dados da Conta
+                </label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+                <label class="form-check-label" for="flexRadioDefault2">
+                    Tudo
+                </label>
+            </div>
+        </div>
+    </div>
+</section>
