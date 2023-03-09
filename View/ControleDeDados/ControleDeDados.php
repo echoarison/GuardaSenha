5b1cc873806159de63d12bfc5e142e41e9121c37
Patric Alison <alisson_gamer2000@outlook.com>
2023-03-09 17:35:01 -0300

    add ControleDeDados.php

--- View/ControleDeDados/ControleDeDados.php
+++ View/ControleDeDados/ControleDeDados.php
@@ -1,1 +1,108 @@
-<h1><marquee>HELLO Word!!</marquee></h1>
+<!--Search-->
+<section class="container-fluid my-3">
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <form class="d-flex mx-auto" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn butonPri" type="submit">Search</button>
+            </form>
+        </div>
+
+    </nav>
+
+    <div class="container-fluid my-2 text-center">
+            <button class="btn butonPri" type="submit">Cadastrar</button>
+    </div>
+
+</section>
+
+<section class="container-fluid my-1">
+    <table class="table text-bg-light text-center">
+        <thead>
+            <tr>
+                <th scope="col" class="tdTitlePri">UserConta</th>
+                <th scope="col" class="tdTitleSec">EmailConta</th>
+                <th scope="col" class="tdTitlePri">SenhaConta</th>
+                <th scope="col" class="tdTitleSec">OrigemConta</th>
+                <th scope="col" class="tdTitlePri">Config Dados</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+            <td scope="row">UsernameCat023</td>
+                <td>teste@teste.com</td>
+                <td>1234+admin</td>
+                <td>Netflix</td>
+                <td>
+                    <button class="btn butonPri" type="button">Detalhes</button>
+                    <button class="btn butonPri" type="button">Alterar</button>
+                </td>
+            </tr>
+            <tr>
+                <td scope="row">UsernameCat023</td>
+                <td>teste@teste.com</td>
+                <td>1234+admin</td>
+                <td>Netflix</td>
+                <td>
+                    <button class="btn butonPri" type="button">Detalhes</button>
+                    <button class="btn butonPri" type="button">Alterar</button>
+                </td>
+            </tr>
+            <tr>
+                <td scope="row">UsernameCat023</td>
+                <td>teste@teste.com</td>
+                <td>1234+admin</td>
+                <td>Netflix</td>
+                <td>
+                    <button class="btn butonPri" type="button">Detalhes</button>
+                    <button class="btn butonPri" type="button">Alterar</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td scope="row">UsernameCat023</td>
+                <td>teste@teste.com</td>
+                <td>1234+admin</td>
+                <td>Netflix</td>
+                <td>
+                    <button class="btn butonPri" type="button">Detalhes</button>
+                    <button class="btn butonPri" type="button">Alterar</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td scope="row">UsernameCat023</td>
+                <td>teste@teste.com</td>
+                <td>1234+admin</td>
+                <td>Netflix</td>
+                <td>
+                    <button class="btn butonPri" type="button">Detalhes</button>
+                    <button class="btn butonPri" type="button">Alterar</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td scope="row">UsernameCat023</td>
+                <td>teste@teste.com</td>
+                <td>1234+admin</td>
+                <td>Netflix</td>
+                <td>
+                    <button class="btn butonPri" type="button">Detalhes</button>
+                    <button class="btn butonPri" type="button">Alterar</button>
+                </td>
+            </tr>
+
+            <tr>
+                <td scope="row">UsernameCat023</td>
+                <td>teste@teste.com</td>
+                <td>1234+admin</td>
+                <td>Netflix</td>
+                <td>
+                    <button class="btn butonPri" type="button">Detalhes</button>
+                    <button class="btn butonPri" type="button">Alterar</button>
+                </td>
+            </tr>
+
+        </tbody>
+    </table>
+</section>
